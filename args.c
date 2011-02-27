9ca2903ae7524187ada6696737696a3073296172
fred <fred@9fca11c9-bd95-42b6-85bf-065c196c5702>
2011-02-27 08:51:00 +0000

    * Refactor args prog (strcmp.c & strcmp.h was added but not used)
    git-svn-id: file:///var/svn/repos/mach-o/mach-o_testing/trunk@106 9fca11c9-bd95-42b6-85bf-065c196c5702

--- args.c
+++ args.c
@@ -1,38 +1,67 @@
 #include "printf.h"
-
+#if 0
+#include "strcmp.h"
+#endif
 extern int write(int fd, char *str, int len);
 extern void exit(int code);
-extern unsigned int get_stack_pointer();
+extern unsigned int *get_stack_pointer();
 
-int main(int argc, char *argv[]) {
-	int i, j;
-	unsigned int *p;
-	char a, b, c, d;
+#ifdef __linux__
+// TBD
+#else
+	#if defined(__ppc__) || defined(__PPC__) || defined(__ppc64__)
+	#elif __i386__
+	#define START_BEGIN 0xBFFFFFF0
+	#define STACK_END 	0xBF800000
+	#elif __x86_64__
+	// FIXME
+	#define START_BEGIN 0x0
+	#define STACK_END 	0x0
+	#elif __arm__
+	#define START_BEGIN 0x2ffff935
+	#define STACK_END 	0x2ffff1c0
+	#endif
+#endif
+
+int main(int argc, char *argv[], char *envp[]) {
+	int i;
 	unsigned int *addr_sp;
-	printf("Start\n");
-#if 1
 	addr_sp = (unsigned int *) get_stack_pointer();
 	printf("Stack pointer: %X\n", (unsigned int) addr_sp); 
 	printf("argc: %d [%X]\n", argc, (unsigned int)&argc); 
 	printf("Stack pointer: %X\n", (unsigned int) addr_sp); 
-#endif
 
-#if 1
+	printf("\n");
 	for(i=0; i < argc; i++)
 		printf("argv[%d] at %08X point to %08X => %s\n", i, 
 			(unsigned int) &argv[i], (unsigned int) argv[i], argv[i]);
 
-#endif
-#if 1
-//	for(p = (unsigned int *)0xBFFFFFF0 ; p > (unsigned int *)0xBF000000; p--) {
-	for(p = (unsigned int *)0x2ffff935 ; p > (unsigned int *)0x2ffff1c0; p--) {
-		j = *p;
-		a = j >> 24;
-		b = j >> 16;
-		c = j >> 8;
-		d = j;
-		printf("at %X => %X : %c %c %c %c\n", (unsigned int)p , (unsigned int)j,
-			a, b, c, d);
+	printf("\n");
+	for(i=0; envp[i]; i++)
+		printf("envp[%d] at %08X point to %08X => %s\n", i, 
+			(unsigned int) &envp[i], (unsigned int) envp[i], envp[i]);
+
+
+#if 0
+	{
+		unsigned int *p;
+		char a, b, c, d;
+
+		printf("\n");
+		for(i=0; envp[i]; i++) {
+			if(strncmp(envp[i], "TERM_PROGRAM", 12) == 0) {
+				
+			}
+		}
+		for(p = (unsigned int *)START_BEGIN; p >= (unsigned int *)STACK_END; p--) {
+			i = *p;
+			a = i >> 24;
+			b = i >> 16;
+			c = i >> 8;
+			d = i;
+			printf("at %X => %X : %c %c %c %c\n", (unsigned int)p ,
+					(unsigned int)i, a, b, c, d);
+		}
 	}
 #endif
 	return 0;
